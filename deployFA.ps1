e790b686615b43e228f07306e651746b6339986a
Damir Zelenic <dzelenic@windowslive.com>
2021-04-08 11:08:32 +0200

    Add app settings

--- deployFA.ps1
+++ deployFA.ps1
@@ -5,21 +5,26 @@ param(
     [parameter()]
     [string]$FunctionResourceGroup,
     [parameter()]
-    [string]$Location
+    [string]$Location,
+    [string]$AppSettingsJSON
 )
 
 $Function = Get-AzFunctionApp -Name $FunctionName -ResourceGroupName $FunctionResourceGroup
-if ($Function) {
+if ($Function)
+{
     Write-Host "FunctionApp $Function already exists"
 }
-else {
+else
+{
     Write-Host "Resource does not exist, creating"
-    if ($null -eq (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -eq "$FunctionResourceGroup" } ) ) {
+    if ($null -eq (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -eq "$FunctionResourceGroup" } ) )
+    {
         Write-Host "Resourcegroup does not exist yet, creating"
         New-AzResourceGroup -Name $FunctionResourceGroup -Location $Location
     }
     $StorageAccountName = "sa$FunctionName".ToLower()
-    if ($null -eq (Get-AzStorageAccount | Where-Object { $_.Name -eq $StorageAccountName } )) {
+    if ($null -eq (Get-AzStorageAccount | Where-Object { $_.Name -eq $StorageAccountName } ))
+    {
         Write-Host "Storageaccount does not exist yet, creating"
         $SAParameters = @{
             Name              = $StorageAccountName
@@ -29,12 +34,29 @@ else {
         }
         New-AzStorageAccount @SAParameters
     }
-    $FAParameters = @{
-        Name               = $FunctionName
-        ResourceGroupName  = $FunctionResourceGroup
-        StorageAccountName = $StorageAccountName
-        Location           = $location
-        Runtime            = "PowerShell"
+    $appSettings = $appSettingsJSON | ConvertFrom-Json -AsHashtable
+
+    if (!$appSettings -or $AppSettings -eq "")
+    {
+        $FAParameters = @{
+            Name               = $FunctionName
+            ResourceGroupName  = $FunctionResourceGroup
+            StorageAccountName = $StorageAccountName
+            Location           = $location
+            Runtime            = "PowerShell"
+            IdentityType       = "SystemAssigned"
+        }
+    }
+    else
+    {
+        $FAParameters = @{
+            Name               = $FunctionName
+            ResourceGroupName  = $FunctionResourceGroup
+            StorageAccountName = $StorageAccountName
+            Location           = $location
+            Runtime            = "PowerShell"
+            IdentityType       = "SystemAssigned"
+            AppSettings        = $appSettings
     }
     $Function = New-AzFunctionApp @FAParameters
 }
