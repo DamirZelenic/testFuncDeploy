186141ca3481d894dbce56db5c324ef9f1fd77f2
Damir Zelenic <dzelenic@windowslive.com>
2021-04-15 11:26:37 +0200

    Add managed identity

--- setRBAC.ps1
+++ setRBAC.ps1
@@ -7,12 +7,20 @@ param(
 )
 
 # Set RBAC for Managed Identities
+# SPN needs User Access Administrator and Directory.Read.All (on old Azure AD Graph) permissions
 $identityId = (Get-AzFunctionApp -Name $FunctionName -ResourceGroupName $FunctionResourceGroup).IdentityPrincipalId
 
 switch ($FunctionName)
 {
     "dumyFuncDZdev"{
-        New-AzRoleAssignment -ObjectId $identityId -RoleDefinitionName Contributor -Scope "/subscriptions/c5afa33c-e037-4c9d-8b04-746ac7bd16c5"
+        try
+        {
+            New-AzRoleAssignment -ObjectId $identityId -RoleDefinitionName Contributor -Scope "/subscriptions/c5afa33c-e037-4c9d-8b04-746ac7bd16c5" -ErrorAction stop
+        }
+        catch
+        {
+            Write-Host "$_"
+        }
         break
     }
 }
